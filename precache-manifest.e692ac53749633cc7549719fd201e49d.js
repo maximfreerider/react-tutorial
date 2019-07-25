97a939bdd5055b1e0474de7c7263ccb8b39e9ddd
maximfreerider <ivanovma41@gmail.com>
2019-07-25 17:45:28 +0300

    Updates

---
+++ precache-manifest.e692ac53749633cc7549719fd201e49d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234115d86b1b63ec1a06c74518b66eb3",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "e8fd562f4da746ee4c5d",
+    "url": "/react-tutorial/static/css/main.c8b44c70.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "e8fd562f4da746ee4c5d",
+    "url": "/react-tutorial/static/js/main.3e6f7443.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
